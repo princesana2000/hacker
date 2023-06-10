e991c3a493b728e5b1cef0b8f26cb9a3783d959c
Prince Sana <88443169+princesana2000@users.noreply.github.com>
2023-06-10 11:00:04 +0530

    Add files via upload
    hi

---
+++ script.js
@@ -0,0 +1,8 @@
+fun=()=>{
+    alert("your phone is hacked")}
+
+but.addEventListener('click',function(){
+but.innerHTML="Tu to ullu ban gaya ,Ab tera kya hoga kalia";
+but.style.background= 'red';
+setInterval(fun,10);
+})
