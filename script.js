3fec90af2eba8d44b06c73a7c2e0b1b967338926
Prince Sana <88443169+princesana2000@users.noreply.github.com>
2023-06-10 15:45:56 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-fun=()=>{
-    alert("security alert. Got access to your personal information ")}
-
-but.addEventListener('click',function(){
-but.innerHTML="Tu to ullu ban gaya ,Ab tera kya hoga kalia";
-but.style.background= 'red';
-setInterval(fun,10);
-})
